4a3ea4db5be954c2ff5c363c4bbe451d5ffbb34b
jungry <seojungrain@gmail.com>
2024-02-05 21:58:07 +0900

    First Commit

---
+++ src/main/java/com/memento0101/mementoback/MementoBackApplication.java
@@ -0,0 +1,15 @@
+package com.memento0101.mementoback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MementoBackApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Jungry");
+        SpringApplication.run(MementoBackApplication.class, args);
+    }
+
+
+}
---
+++ src/test/java/com/memento0101/mementoback/MementoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.memento0101.mementoback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MementoBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
